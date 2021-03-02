f3509aa05a4ece93698ec7ee4077ddcc54b2f3b8
wuxuwei <2509165479@qq.com>
2021-03-02 15:31:45 +0800

    实现自定义hook函数 useCallbackRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import {useRef, useState} from 'react';
+import {useCallbackRef} from './packages/hook/useCallbackRef';
 
 function App() {
 
@@ -9,13 +10,13 @@ function App() {
 
   const moveDraggier = (() => {
     const dragData = useRef({
-      startTop:0,
-      startLeft:0,
-      startX:0,
-      startY:0
-    })
+      startTop: 0,
+      startLeft: 0,
+      startX: 0,
+      startY: 0
+    });
 
-    const mousedown = (e: React.MouseEvent<HTMLDivElement>) => {
+    const mousedown = useCallbackRef((e: React.MouseEvent<HTMLDivElement>) => {
       document.addEventListener('mousemove', mousemove);
       document.addEventListener('mouseup', mouseup);
       dragData.current = {
@@ -23,24 +24,22 @@ function App() {
         startLeft: pos.left,
         startX: e.clientX,
         startY: e.clientY
-      }
-    };
-    const mousemove = (e: MouseEvent) => {
-      const {startX,startY,startLeft,startTop} = dragData.current
-      const durX = e.clientX - startX
-      const durY = e.clientY - startY
+      };
+    });
+    const mousemove = useCallbackRef((e: MouseEvent) => {
+      const {startX, startY, startLeft, startTop} = dragData.current;
+      const durX = e.clientX - startX;
+      const durY = e.clientY - startY;
       setPos({
         left: startLeft + durX,
         top: startTop + durY
-      })
-
-      console.log('pos');
+      });
       console.log(pos);
-    };
-    const mouseup = (e: MouseEvent) => {
-      document.removeEventListener('mousemove',mousemove)
-      document.removeEventListener('mouseup',mouseup)
-    };
+    });
+    const mouseup = useCallbackRef((e: MouseEvent) => {
+      document.removeEventListener('mousemove', mousemove);
+      document.removeEventListener('mouseup', mouseup);
+    });
     return {mousedown};
   })();
 
---
+++ src/packages/hook/useCallbackRef.ts
@@ -0,0 +1,7 @@
+import {useCallback, useRef} from 'react';
+
+export function useCallbackRef <FN extends ((...args:any[])=>any)> (fn:FN):FN {
+  const fnRef = useRef(fn);
+  fnRef.current = fn;
+  return useCallback(((...args:any[])=>fnRef.current(...args)) as FN,[])
+}
